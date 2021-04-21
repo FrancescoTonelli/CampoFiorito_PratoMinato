3234472edb7f519ca81bde5fdd940ec8c81e5ada
FrancescoTonelli <71379692+FrancescoTonelli@users.noreply.github.com>
2021-04-21 10:49:18 +0200

    Trap

---
+++ PratoFiorito/PratoFiorito/Campo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PratoFiorito
+{
+    public class Campo
+    {
+        private int[,] _griglia;
+
+        public Campo()
+        {
+            Griglia = new int[8, 8];
+            Riempi();
+        }
+
+        public int[,] Griglia
+        {
+            get
+            {
+                return _griglia;
+            }
+            set
+            {
+                _griglia = Griglia;
+            }
+        }
+
+        public void Riempi()
+        {
+            int i = 10;
+
+            while (i != 0)
+            {
+
+            }
+        }
+    }
+}
